fd1fd15ae54c9019b347e296eb0332989a10c0a8
faisalm-ag <faisalmuhamadabdulghani@gmail.com>
2025-12-10 11:12:00 +0700

    Upload pertama: Streamlit Sentiment App

---
+++ app.py
@@ -0,0 +1,33 @@
+import joblib
+import streamlit as st
+
+# Load Model & Vectorizer
+model = joblib.load(r"C:\Users\faisa\OneDrive\Dokumen\Tugas Kuliah\Faisal\NLP\streamlit\models\model_logistic_regression.pkl")
+tfidf = joblib.load(r"C:\Users\faisa\OneDrive\Dokumen\Tugas Kuliah\Faisal\NLP\streamlit\models\tfidf_vectorizer.pkl")
+
+# Tampilan Aplikasi
+st.title("Aplikasi Analisis Sentimen Komentar - ファイサル")
+st.write("Aplikasi ini digunakan untuk memprediksi apakah sebuah komentar bernada **positif** atau **negatif** menggunakan model Logistic Regression yang sudah dilatih sebelumnya.")
+
+# Input dari user
+komentar = st.text_input("Masukkan komentar yang ingin dianalisis")
+
+# Tombol submit
+if st.button("Submit"):
+    if komentar.strip() == "":
+        st.warning("Komentar tidak boleh kosong.")
+    else:
+        # Transform dan prediksi
+        vector = tfidf.transform([komentar])
+        prediksi = model.predict(vector)[0]
+
+        label_map = {
+            0: "Negatif",
+            1: "Positif"
+        }
+
+        hasil = label_map.get(prediksi, "Tidak Dikenal")
+
+        st.subheader("Hasil Analisis Sentimen")
+        st.write(f"**Komentar:** {komentar}")
+        st.write(f"**Prediksi Sentimen:** {hasil}")
